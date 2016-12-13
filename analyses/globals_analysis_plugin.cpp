675dc294c5730e19164777d494364a036e939cf5
Martina Vitovska <409920@mail.muni.cz>
2016-12-13 21:23:09 +0100

    Implement more complex globals analysis

--- analyses/globals_analysis_plugin.cpp
+++ analyses/globals_analysis_plugin.cpp
@@ -2,20 +2,62 @@
 #include <llvm/IR/Value.h>
 #include <llvm/Support/raw_os_ostream.h>
 #include "instr_plugin.hpp"
+#include <llvm/IR/Instructions.h>
 
 class GlobalsAnalysisPlugin : public InstrPlugin{
+	private:
+	 llvm::Module* M;
 	public:
 	 GlobalsAnalysisPlugin(llvm::Module* module) {
+		M = module;
 		// llvm::errs() << "Running analysis on global variables...\n";
 	 }
 
-	 bool isUsed(llvm::Value *globalVariable){
-		 if(globalVariable->getNumUses() > 0){
-			 return true;
-		 }
-		 else{
+	 bool isGlobal(llvm::Value* variable){
+		llvm::Module::global_iterator GI = M->global_begin(), GE = M->global_end();
+		for ( ; GI != GE; ++GI) {
+		  llvm::GlobalVariable *GV = llvm::dyn_cast<llvm::GlobalVariable>(GI);
+		  if (!GV) continue;
+		  if(GV == variable->stripPointerCasts()->stripInBoundsOffsets() || GV == variable){
+		    return true;
+		  }
+		}
+	   
+		 //if(!(llvm::dyn_cast<llvm::GlobalVariable>(variable)))
+			  //return false;
+
+		 return false;
+	 }
+
+	 bool isUsed(llvm::Value *variable){
+		 // Check if variable is global variable
+		 if(!(llvm::dyn_cast<llvm::GlobalVariable>(variable))){
 			 return false;
 		 }
+
+		 // variable is not used anywhere
+		 if(variable->getNumUses() == 0){
+			 return false;
+		 }
+		 else{
+			 for (auto I = variable->use_begin(), E = variable->use_end(); I != E; ++I) {
+				#if (LLVM_VERSION_MINOR < 5)
+					const llvm::Value *use = *I;
+				#else
+					const llvm::Value *use = I->getUser();
+				#endif
+				const llvm::StoreInst *SI = llvm::dyn_cast<llvm::StoreInst>(use);
+				if (SI && SI->getValueOperand()->stripPointerCasts() == variable) {
+					return true;
+				}
+				const llvm::LoadInst *LI = llvm::dyn_cast<llvm::LoadInst>(use);
+				if(LI && LI->getOperand(0)->stripPointerCasts() == variable){
+				  return true;
+				}
+			}
+		 }
+
+		 return false;
 	 }
 };
 
